c22eb961caafdc241b541cde31673376503fa7a9
MARTINDLB15 <mdlondonob@unal.edu.co>
2025-07-24 22:51:53 -0500

    Piloto commit de mi appAves Flask

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename 
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
 @app.route('/')
@@ -13,13 +17,37 @@ def index():
 def guia():
     return render_template ('guia.html')
 
-@app.route('/registro')
+lista_registros = []
+@app.route('/registro',  methods=['GET', 'POST'])
 def registro():
+    if request.method == 'POST':
+        nombre_completo = request.form.get('nombre_completo')
+        nombre_comun = request.form.get('nombre_comun')
+        nombre_cientifico = request.form.get('nombre_cientifico')
+        comentario = request.form.get('comentario')
+        imagen = request.files.get('imagen')
+
+        ruta_imagen = None
+        if imagen and imagen.filename != '':
+            nombre_archivo = imagen.filename
+            ruta_absoluta = os.path.join(UPLOAD_FOLDER, nombre_archivo)
+            imagen.save(ruta_absoluta)
+            ruta_imagen = f'uploads/{nombre_archivo}'
+        
+        lista_registros.append({
+            'nombre_completo': nombre_completo,
+            'nombre_comun': nombre_comun,
+            'nombre_cientifico': nombre_cientifico,
+            'comentario': comentario,
+            'imagen': ruta_imagen
+        })
+        return redirect(url_for('explora'))
+
     return render_template ('registro.html')
 
 @app.route('/explora')
 def explora():
-    return render_template ('explora.html')
+    return render_template ('explora.html', registros=lista_registros)
 
 @app.route('/conoce')
 def conoce():
